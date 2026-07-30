102cf21b9e2471affd48bc2d5965a32f3e025de7
Zuhair <zuhairanns@gmail.com>
2026-07-30 17:22:12 +0500

    fix bug1

--- app/fonts.js
+++ app/fonts.js
@@ -4,5 +4,5 @@ export const gulzar = Gulzar({
   subsets: ['arabic'], // Gulzar uses Arabic/Urdu script
   weight: '400',
   style: 'normal',
-  variable: '--font-gulzar', // optional, for CSS variable usage
+  variable: '--font-gulzar', 
 })
